104a590487b2a700887ba3851622bdf07cd32a8d
Jonathan Harwood <jhharwood88@gmail.com>
2018-12-20 21:20:30 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,11 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+end
+
+
+class Artist
   attr_accessor :name
   attr_reader :songs
 
@@ -17,14 +22,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
